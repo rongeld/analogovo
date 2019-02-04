9174207997915175c44a7232a1bdd54574d12273
rongeld <andrew.zakrevskiy@gmail.com>
2019-02-04 14:31:35 +0100

    config/keys, mongoose connection to mongodb

---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+    mongoURI: "mongodb://andrew:andrew1@ds263571.mlab.com:63571/analogovo"
+  };
--- server.js
+++ server.js
@@ -1,6 +1,17 @@
 const express = require("express");
+const mongoose = require("mongoose");
+
 
 const app = express();
+
+// DB CONFIG
+const db = require("./config/keys").mongoURI;
+
+// Connect to MongoDB
+mongoose
+  .connect(db)
+  .then(() => console.log("MONGODB CONNECTED"))
+  .catch(err => console.log(err));
 
 app.get("/", (req, res) => res.send("Hello!"));
 
