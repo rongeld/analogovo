dfb9ca13ff12f42e157394aebd84eaa194b892e9
rongeld <andrew.zakrevskiy@gmail.com>
2019-02-06 11:43:46 +0100

    validation (errors beginning)

--- routes/api/users.js
+++ routes/api/users.js
@@ -7,6 +7,9 @@ const jwt = require('jsonwebtoken');
 const keys = require('../../config/keys');
 const passport = require('passport');
 
+// LOAD INPUT VALIDAITION
+const validateRegisterInput = require('../../validation/register');
+
 // Load User model
 const User = require("../../models/User");
 
@@ -20,9 +23,18 @@ router.get("/test", (req, res) => res.json({ msg: "Users Works" }));
 // @access  Public
 
 router.post("/register", (req, res) => {
+
+  const {errors, isValid} = validateRegisterInput(req.body);
+
+  // Check validation
+  if (!isValid) {
+    return res.status(400).json(errors)
+  }
+
     User.findOne({ email: req.body.email }).then(user => {
       if (user) {
-        return res.status(400).json({ email: "Email already exists" });
+        errors.email = 'Email already exists'
+        return res.status(400).json(errors);
       } else {
         const newUser = new User({
           name: req.body.name,
