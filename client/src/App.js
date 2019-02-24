942304bfd5de34b6928c43bacaa91ff398445521
rongeld <andrew.zakrevskiy@gmail.com>
2019-02-24 21:29:06 +0100

    Redux store & Chrome extention (redux, react-redux, thunk)

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,30 @@
 import React, { Component } from 'react';
 import {BrowserRouter, Route} from 'react-router-dom';
+import store from './store';
+
 
 import Landing from './components/layout/Landing';
 import Login from './components/auth/Login';
 
+import {Provider} from 'react-redux';
+
 import './App.css';
+
+
 
 class App extends Component {
   render() {
     return (
-      <BrowserRouter>
-        <div className="App">
-          <Route exact path='/' component={Landing}/>
-          <div className="containerApp">
-            <Route exact path="/login" component={Login} />
+      <Provider store={store}>
+        <BrowserRouter>
+          <div className="App">
+            <Route exact path='/' component={Landing}/>
+            <div className="containerApp">
+              <Route exact path="/login" component={Login} />
+            </div>
           </div>
-        </div>
-      </BrowserRouter>
+        </BrowserRouter>
+      </Provider>
     );
   }
 }
